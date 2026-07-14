9cd5561bdcb413930ab0482509b18a1d5c74921c
AkibAshfaq <akibash.dev@gmail.com>
2026-07-14 18:24:13 +0600

    Resotred from the files

---
+++ Module Handling/main.js
@@ -0,0 +1,9 @@
+const math = require('./math.js');
+
+console.log(math.add(5, 3)); // Output: 8
+console.log(math.subtract(5, 3)); // Output: 2
+console.log(math.multiply(5, 3)); // Output: 15
+console.log(math.divide(5, 3)); // Output: 1.6666666666666667
+
+// Used with expoets.add1 = (a, b) => a + b; in math.js
+// console.log(math.add1(5, 3)); // Output: 8
