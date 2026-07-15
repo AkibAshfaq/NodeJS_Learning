84f1f1661474b3727495d6d1c4f24b26dcef520f
AkibAshfaq <akibash.dev@gmail.com>
2026-07-15 06:48:26 +0600

    Update

---
+++ URL_Shortener/routers/staticrouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const {  } = require('../controllers/staticrouter');
+
+router.get('/home', handleHome)
+
+router.route("/")
+.get()
+
+module.exports = router;
